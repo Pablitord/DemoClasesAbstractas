9ea5aebafa992cb7e21023d6a3defee9fadcf7f0
Juan Pablo <rovayojuanpablo@gmail.com>
2024-12-12 23:21:45 -0500

    Update: Compared Figures class added

--- src/ClasesAbstractas/CompararFiguras.java
+++ src/ClasesAbstractas/CompararFiguras.java
@@ -1,5 +1,17 @@
 package ClasesAbstractas;
 
 public class CompararFiguras {
+	
+	public static FiguraGeometrica compararFiguras(FiguraGeometrica figura1, FiguraGeometrica figura2) {
+		
+		if (figura1.calcularArea() > figura2.calcularArea()) {
+			
+			return figura1;
+		} else {
+			
+			return figura2;
+		}
+		
+	}
 
 }
